2c7ebf67c5c29fc1b583414456b806dcc2223d15
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2020-10-04 10:18:46 +0900

    Updates

---
+++ precache-manifest.0af2fe88138ea2d1b4f581741cec81e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "739fd9f8be67bb7bb8b6f1020572c3a0",
+    "url": "/react-test-chooser/index.html"
+  },
+  {
+    "revision": "4d511df0cab66f59fc7e",
+    "url": "/react-test-chooser/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4c7ac81f6af7ef4ab42c",
+    "url": "/react-test-chooser/static/js/2.1d38767d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test-chooser/static/js/2.1d38767d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d511df0cab66f59fc7e",
+    "url": "/react-test-chooser/static/js/main.ef76101b.chunk.js"
+  },
+  {
+    "revision": "366606561b3e61c96f71",
+    "url": "/react-test-chooser/static/js/runtime-main.d7316be1.js"
+  }
+]);
